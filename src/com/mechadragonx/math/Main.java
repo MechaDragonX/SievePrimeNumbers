d126927cf1cc97b40d5edac3e06fac61a492057a
MechaDragonX <sonicraghav@gmail.com>
2019-11-11 16:54:56 -0800

    Added isPrime() function

--- src/com/mechadragonx/math/Main.java
+++ src/com/mechadragonx/math/Main.java
@@ -7,34 +7,45 @@ public class Main
 {
     public static void main(String[] args)
     {
-        long start;
-        long end;
+//        long start;
+//        long end;
+//
+//        // Without Squaring
+//        System.out.println("Without Squaring:\n30");
+//        start = System.nanoTime();
+//        primes(30);
+//        end = System.nanoTime();
+//        System.out.println((((double)(end - start) / 1E+6) + " ms"));
+//
+//        System.out.println("121");
+//        start = System.nanoTime();
+//        primes(121);
+//        end = System.nanoTime();
+//        System.out.println((((double)(end - start) / 1E+6) + " ms"));
+//
+//        // Without Squaring
+//        System.out.println("\nWith Squaring:\n30");
+//        start = System.nanoTime();
+//        primesSquared(30);
+//        end = System.nanoTime();
+//        System.out.println((((double)(end - start) / 1E+6) + " ms"));
+//
+//        System.out.println("121");
+//        start = System.nanoTime();
+//        primes(121);
+//        end = System.nanoTime();
+//        System.out.println((((double)(end - start) / 1E+6) + " ms"));
 
-        // Without Squaring
-        System.out.println("Without Squaring:\n30");
-        start = System.nanoTime();
-        primes(30);
-        end = System.nanoTime();
-        System.out.println((((double)(end - start) / 1E+6) + " ms"));
-
-        System.out.println("121");
-        start = System.nanoTime();
-        primes(121);
-        end = System.nanoTime();
-        System.out.println((((double)(end - start) / 1E+6) + " ms"));
-
-        // Without Squaring
-        System.out.println("\nWith Squaring:\n30");
-        start = System.nanoTime();
-        primesSquared(30);
-        end = System.nanoTime();
-        System.out.println((((double)(end - start) / 1E+6) + " ms"));
-
-        System.out.println("121");
-        start = System.nanoTime();
-        primes(121);
-        end = System.nanoTime();
-        System.out.println((((double)(end - start) / 1E+6) + " ms"));
+        System.out.println("2: " + isPrime(2));
+        System.out.println("4: " + isPrime(4));
+        System.out.println("3: " + isPrime(3));
+        System.out.println("7: " + isPrime(7));
+        System.out.println("10: " + isPrime(10));
+        System.out.println("21: " + isPrime(21));
+        System.out.println("37: " + isPrime(37));
+        System.out.println("45: " + isPrime(45));
+        System.out.println("83: " + isPrime(83));
+        System.out.println("113: " + isPrime(113));
     }
     private static Queue<Integer> prepare(Queue<Integer> numbers, int number)
     {
@@ -101,5 +112,40 @@ public class Main
         Queue<Integer> result = prepare(numbers, number);
         sieveSquared(numbers, result, number);
         return result;
+    }
+
+    private static boolean squaredCheck(Queue<Integer> numbers, Queue<Integer> result, int number)
+    {
+        int prime = 0;
+        int current = 0;
+        int first= 0;
+        while(prime < Math.sqrt(number))
+        {
+            if(number == numbers.peek()) return true;
+            else prime = numbers.remove();
+            result.add(prime);
+            first = numbers.peek();
+            for(int i = 0; current < numbers.peek(); i++)
+            {
+                if((numbers.peek() >= (prime * prime)) || (current != first || i == 0))
+                {
+                    current = numbers.remove();
+                    if(current % prime != 0) numbers.add(current);
+                    else
+                    {
+                        if(current == number) return false;
+                    }
+                }
+                else numbers.add(numbers.remove());
+            }
+            current = 0;
+        }
+        return true;
+    }
+    private static boolean isPrime(int number)
+    {
+        Queue<Integer> numbers = new LinkedList<>();
+        Queue<Integer> result = prepare(numbers, number);
+        return squaredCheck(numbers, result, number);
     }
 }
